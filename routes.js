27a14a4bc39bc1a91a9c32ba90792de0f3884921
Nazaret Arroyo <nazaretarroyo@gmail.com>
2024-10-29 11:47:43 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,17 +1,9 @@
 'use strict';
 import settings from './settings.js';
 import express, {request, response, Router} from 'express';
-const router = express.Router(); // das Methode Router von express ist um routen anzulegen
+const router = express.Router(); 
 
-
-router.get('/loadData', (request, response) => { //pfad für die daten settings.data
-    response.json(settings.data); // data aus der Settings holen bzw das Array asu der Json datei 
-    // hier wird data heraus genommen und per JSON gelifert
+router.get('/loadData', (request, response) => { 
+    response.json(settings.data); 
 })
-
- //route angelegt
- // im server gebunden
- // das ermöglich von client mit hilfe eine AJAX Anfrage die Daten aus der JSON auslesen
-
-
 export default router;
