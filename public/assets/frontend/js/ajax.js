499ff67c2ddd67d7c4c3936a58e8237911e8e979
Nazaret Arroyo <nazaretarroyo@gmail.com>
2024-10-29 11:42:33 +0100

    Update ajax.js

--- public/assets/frontend/js/ajax.js
+++ public/assets/frontend/js/ajax.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const ajax = {
-    loadJSON(url){ //bekommt den pfad '/loadData' durch fecht als argument ->pfad in settings.js -urlData
+    loadJSON(url){ 
         return fetch(url).then(
-            res => res.json() // wenn ein ergebnis - per json umwandeln
+            res => res.json()
             
         )
     }
 }
-//die url wir übergeben wenn die funktion loadjson aufgerufen wird -> index.js
+
 export default ajax;
